3d1819597f64210d7035f4944178a93abbcc6e2e
CodinGitHub <gms2programador@gmail.com>
2022-10-04 10:04:27 -0500

    clase 1 - repaso general

---
+++ Clase 1 - Repaso/main.js
@@ -0,0 +1,51 @@
+var letras;
+let numero = 'este es un numero';
+const FRUTAS = 'banano';
+
+// numero = prompt('ingresa tu edad');
+// FRUTAS = 'fresa';
+letras = true;
+
+console.log(numero);
+console.log(typeof(numero));
+
+//Condicionales
+if(numero === 15){
+    console.log('Si es igual');
+}else{
+    console.log('NO es igual');
+}
+
+//Operadores = == === > < + - / *
+if(numero <= 10){
+    console.log('Si es mayor a 10');
+}else{
+    console.log('NO es mayor a 10');
+}
+
+//Bucles
+let valorInicial = 0;
+
+// for(let i=3; i<5; i++){
+//     valorInicial++;
+//     console.log(valorInicial)
+// }
+
+// let i=6
+// while(i<5){
+//     valorInicial++;
+//     console.log(valorInicial);
+//     i++;
+// }
+
+// do {
+//     valorInicial++;
+//     console.log(valorInicial);
+//     i++;
+// } while (i<5);
+
+let i = 3;
+
+while (i) {
+  console.log( i-- );
+}
